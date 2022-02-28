b4a9fa3008a8371a480b6d14c3a89786e712d365
f4ilfish <mishka4ernij@mail.ru>
2022-02-28 14:50:26 +0700

    feature(LabWork1): exception description

--- LabWork1/Model/ObjectNotFoundException.cs
+++ LabWork1/Model/ObjectNotFoundException.cs
@@ -2,8 +2,15 @@
 
 namespace Model
 {
+    /// <summary>
+    /// Class absence an object into collection
+    /// </summary>
     public class ObjectNotFoundException : Exception
     {
+        /// <summary>
+        /// Exception of absence an object
+        /// </summary>
+        /// <param name="message">Exception message</param>
         public ObjectNotFoundException(string message) : base(message) { }
     }
 }

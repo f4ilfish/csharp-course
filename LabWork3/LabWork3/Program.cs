d4c7bac663b9998994c952f7e22396eefc0c7ad7
f4ilfish <mishka4ernij@mail.ru>
2022-03-23 23:18:44 +0700

    feature(LabWork3): add figure's classes and methods

---
+++ LabWork3/LabWork3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Model;
+
+namespace View
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var figureList = new List<FigureBase>();
+            var rnd = new Random();
+
+            for (var i = 0; i < 10; i++)
+            {
+                var choseFigure = rnd.Next(3);
+                switch (choseFigure)
+                {
+                    case 0:
+                        figureList.Add(Sphere.GetRandomSphere());
+                        break;
+                    case 1:
+                        figureList.Add(Pyramid.GetRandomPyramid());
+                        break;
+                    case 2:
+                        figureList.Add(Parallelepiped.GetRandomParallelepiped());
+                        break;
+                    default:
+                        Console.WriteLine("Unknown chose of figure");
+                        break;
+                }
+            }
+
+            ColorfulPrint(">>> Created figures:", ConsoleColor.Yellow);
+            foreach (var tmpFigure in figureList)
+            {
+                Console.WriteLine(tmpFigure.GetFigureInfo());
+            }
+
+            ColorfulPrint(">>> Volumes of figures:", ConsoleColor.Yellow);
+            foreach (var tmpFigure in figureList)
+            {
+                Console.WriteLine($"{tmpFigure.GetType()} have {tmpFigure.GetVolume()} volume");
+            }
+        }
+
+        /// <summary>
+        /// Method to print colorful text
+        /// </summary>
+        /// <param name="text">Input text</param>
+        /// <param name="color">Color</param>
+        private static void ColorfulPrint(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($">>> {text}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ LabWork3/Model/FigureBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Model
+{
+    /// <summary>
+    /// Abstract class figure
+    /// </summary>
+    public abstract class FigureBase
+    {
+        /// <summary>
+        /// Min figure param value
+        /// </summary>
+        protected const double MinFigureParamValue = 0;
+
+        /// <summary>
+        /// Abstract method to get figure volume
+        /// </summary>
+        /// <returns></returns>
+        public abstract double GetVolume();
+
+        /// <summary>
+        /// Abstract method to get figure info
+        /// </summary>
+        /// <returns></returns>
+        public abstract string GetFigureInfo();
+
+        /// <summary>
+        /// Check positive value
+        /// </summary>
+        /// <param name="value">Input value</param>
+        /// <param name="minValue">Minimum allow value</param>
+        protected static void CheckValue(double value, double minValue)
+        {
+            if (value < minValue)
+            {
+                throw new ArgumentOutOfRangeException(
+                    $"{value} must be greater than {minValue}");
+            }
+        }
+    }
+}

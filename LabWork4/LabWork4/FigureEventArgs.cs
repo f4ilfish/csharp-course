d7db296f0ddcd2ba42395c6916d928b207bef4c0
f4ilfish <mishka4ernij@mail.ru>
2022-04-25 14:41:17 +0700

    doc(LabWork4) add xml event

--- LabWork4/LabWork4/FigureEventArgs.cs
+++ LabWork4/LabWork4/FigureEventArgs.cs
@@ -3,11 +3,20 @@ using Model;
 
 namespace View
 {
-    //TODO: XML
+    /// <summary>
+    /// FigureEventArgs class
+    /// </summary>
     public class FigureEventArgs : EventArgs
     {
+        /// <summary>
+        /// Added figure auto-prop
+        /// </summary>
         public FigureBase Figure { get; }
 
+        /// <summary>
+        /// FigureEventArgs instance constructor
+        /// </summary>
+        /// <param name="figure"></param>
         public FigureEventArgs(FigureBase figure)
         {
             Figure = figure;
